38ea74deb123e65c5919da7df4f9d38ca2bb943a
Jonathan Welton <j.welton@audionetwork.com>
2021-06-10 16:35:53 +0100

    Revert "Added Rollbar to the custom error page"
    This reverts commit 6bad73f3da1657625f82260d0f18e7fea7515ba5.

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,3 @@
-import Rollbar from 'rollbar'
-
 function Error({ statusCode }) {
   return (
     <p>
@@ -12,16 +10,6 @@ function Error({ statusCode }) {
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-
-	const rollbar = new Rollbar({
-		accessToken: `${process.env.ROLLBAR_SERVER_TOKEN}`,
-		captureUncaught: true,
-		captureUnhandledRejections: true,
-		environment: process.env.NODE_ENV,
-	})
-
-  rollbar.error(`Error ${statusCode}: ` + err, req)
-
   return { statusCode }
 }
 
