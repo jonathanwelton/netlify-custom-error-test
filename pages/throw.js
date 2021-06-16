5148aa9c9799ff233ad3527d1cea9ba0a35c641e
Jonathan Welton <j.welton@audionetwork.com>
2021-06-16 11:32:23 +0100

    Updated the throw page to include an errorCode prop, which is explicitly passed to the Error page

--- pages/throw.js
+++ pages/throw.js
@@ -1,6 +1,19 @@
-const Throw = () => <div>The error page</div>;
+const Throw = ({ errorCode }) => {
+  if (errorCode) {
+    return <Error statusCode={errorCode} />
+  }
+
+  return <div>No error was thrown</div>
+}
 
 export const getServerSideProps = async() => {
-  throw new Error('a new error')
+  try {
+    throw new Error('a new error')
+  } catch(e) {
+    return {
+      props: { errorCode: 500 }
+    }
+  }
 }
-export default Throw;
+
+export default Throw
